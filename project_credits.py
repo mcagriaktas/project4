6c0b090916a091ed32511874f123250550ea8e26
root <root@10.0.2.15>
2023-10-29 04:00:00 +0300

    final scrips

---
+++ project_credits.py
@@ -0,0 +1,95 @@
+import findspark
+findspark.init("/opt/spark/")
+
+from pyspark.sql import * 
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import time
+import os 
+
+accessKeyId = os.environ.get("AWS_ACCESS_KEY_ID_S3")
+secretAccessKey = os.environ.get("AWS_SECRET_ACCESS_KEY_S3")
+
+if accessKeyId is None or secretAccessKey is None:
+    raise ValueError("AWS access key or secret key not set in environment variables.")
+
+spark = SparkSession.builder \
+.appName("Project") \
+.master("local[2]") \
+.config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0,io.delta:delta-core_2.12:2.4.0") \
+.config("fs.s3a.access.key", accessKeyId) \
+.config("fs.s3a.secret.key", secretAccessKey) \
+.config("fs.s3a.path.style.access", True) \
+.config("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+.config("fs.s3a.endpoint", "s3.amazonaws.com") \
+.getOrCreate()
+
+
+## EXTRACT
+
+df_credits = spark.read.parquet("s3a://tmdb-bronze/credits/")
+
+## TRANSFORM
+
+cast_schema = ArrayType(StructType([
+        StructField('cast_id', IntegerType(), True),
+        StructField('character', StringType(), True),
+        StructField('credit_id', StringType(), True),
+        StructField('gender', IntegerType(), True),
+        StructField('id', IntegerType(), True),
+        StructField('name', StringType(), True),
+]))
+
+df_credits_cast = df_credits.withColumn("cast", from_json(col("cast"), cast_schema))
+
+df_credits_cast = df_credits_cast.select("movie_id", "title", explode(col("cast")).alias("cast"), "crew")
+
+df_credits_cast = df_credits_cast.withColumn("cast_id", col("cast.cast_id"))
+df_credits_cast = df_credits_cast.withColumn("character", col("cast.character"))
+df_credits_cast = df_credits_cast.withColumn("credit_id", col("cast.credit_id"))
+df_credits_cast = df_credits_cast.withColumn("gender", col("cast.gender"))
+df_credits_cast = df_credits_cast.withColumn("id", col("cast.id"))
+df_credits_cast = df_credits_cast.withColumn("name", col("cast.name"))
+
+df_credits_cast = df_credits_cast.select("movie_id", "title", col("cast.*"))
+
+df_credits_cast = df_credits_cast.withColumn("credit_id", when(col("credit_id").isNull(), 0000000000)
+                                             .otherwise(col("credit_id")))
+
+crew_schema = ArrayType(StructType([
+        StructField("credit_id", StringType(), True),
+        StructField("department", StringType(), True),
+        StructField("gender", IntegerType(), True),
+        StructField("id", IntegerType(), True),
+        StructField("job", StringType(), True),
+        StructField("name", StringType(), True),
+]))
+
+df_credits_crew = df_credits.withColumn("crew", from_json(col("crew"), crew_schema))
+
+df_credits_crew = df_credits_crew.select("movie_id", "title", explode(col("crew")).alias("crew"))
+
+df_credits_crew = df_credits_crew.withColumn("credit_id", col("crew.credit_id"))
+df_credits_crew = df_credits_crew.withColumn("department", col("crew.department"))
+df_credits_crew = df_credits_crew.withColumn("gender", col("crew.gender"))
+df_credits_crew = df_credits_crew.withColumn("id", col("crew.id"))
+df_credits_crew = df_credits_crew.withColumn("job", col("crew.job"))
+df_credits_crew = df_credits_crew.withColumn("name", col("crew.name"))
+
+df_credits_crew = df_credits_crew.select("movie_id", "title", col("crew.*"))
+
+df_credits_crew = df_credits_crew.withColumn("credit_id", when(col("credit_id").isNull(), 0000000000)
+                                             .otherwise(col("credit_id")))
+
+## LOAD
+
+dataframes = [df_credits_cast, df_credits_crew]
+
+table_folders = ["CreditsCast", "CreditsCrew"]
+
+for i, dataframe in enumerate(dataframes):
+    time.sleep(5)
+    table_folder_name = table_folders[i]
+    dataframe.write.format("delta").mode("overwrite").save(f"s3a://tmdb-silver/{table_folder_name}")
+
+spark.stop()
---
+++ project_movies.py
@@ -0,0 +1,141 @@
+import findspark
+findspark.init("/opt/spark/")
+
+from pyspark.sql import * 
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import time
+import os
+
+accessKeyId = os.environ.get("AWS_ACCESS_KEY_ID_S3")
+secretAccessKey = os.environ.get("AWS_SECRET_ACCESS_KEY_S3")
+
+if accessKeyId is None or secretAccessKey is None:
+    raise ValueError("AWS access key or secret key not set in environment variables.")
+
+spark = SparkSession.builder \
+.appName("Project") \
+.master("local[2]") \
+.config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0,io.delta:delta-core_2.12:2.4.0") \
+.config("fs.s3a.access.key", accessKeyId) \
+.config("fs.s3a.secret.key", secretAccessKey) \
+.config("fs.s3a.path.style.access", True) \
+.config("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+.config("fs.s3a.endpoint", "s3.amazonaws.com") \
+.getOrCreate()
+
+
+# EXTRACT
+
+df_movies = spark.read.parquet("s3a://tmdb-bronze/movies/")
+
+df_movies = df_movies.withColumn("movie_id", col("id").cast(StringType()))
+
+df_movies_movies = df_movies.select(
+        col("movie_id"),
+        col("title"),
+        col("budget").cast(DoubleType()),
+        col("homepage"),
+        col("original_language"),
+        col("original_title"),
+        col("overview"),
+        col("popularity").cast(FloatType()),
+        col("release_date").cast(DateType()),
+        col("revenue").cast(DoubleType()),
+        col("runtime").cast(IntegerType()),
+        col("status"),
+        col("tagline"),
+        col("vote_average").cast(FloatType()),
+        col("vote_count").cast(IntegerType())
+    )
+
+movies_genres_schema = ArrayType(StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True)
+]))
+
+## TRANSFROM
+
+df_movies_genres = df_movies.withColumn("genres", from_json(col("genres"), movies_genres_schema))
+
+df_movies_genres = df_movies_genres.select("movie_id", explode(col("genres")).alias("genres"))
+
+df_movies_genres = df_movies_genres.select("movie_id", col("genres.*"))
+
+df_movies_genres = df_movies_genres.withColumn("id", when(col("id").isNull(), -9999)
+                                             .otherwise(col("id")))
+
+movies_keywords_schema = ArrayType(StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True)
+]))
+
+df_movies_keywords = df_movies.withColumn("keywords", from_json(col("keywords"), movies_keywords_schema))
+
+df_movies_keywords = df_movies_keywords.select("movie_id", explode(col("keywords")).alias("keywords"))
+
+df_movies_keywords = df_movies_keywords.select("movie_id", col("keywords.*"))
+
+df_movies_keywords = df_movies_keywords.withColumn("id", when(col("id").isNull(), -9999)
+                                             .otherwise(col("id")))
+
+movies_production_companies_schema = ArrayType(StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True)  
+]))
+
+df_movies_production_companies = df_movies.withColumn("production_companies", from_json(col("production_companies"), movies_production_companies_schema))
+
+df_movies_production_companies = df_movies_production_companies.select("movie_id", explode(col("production_companies")).alias("production_companies"))
+
+df_movies_production_companies = df_movies_production_companies.select("movie_id", col("production_companies.*"))
+
+df_movies_production_companies = df_movies_production_companies.withColumn("id", when(col("id").isNull(), -9999)
+                                             .otherwise(col("id")))
+
+movies_production_countries_schema = ArrayType(StructType([
+        StructField("iso_3166_1", StringType(), True),
+        StructField("name", StringType(), True)
+]))
+
+df_movies_production_countries = df_movies.withColumn("production_countries", from_json(col("production_countries"), movies_production_countries_schema))
+
+df_movies_production_countries = df_movies_production_countries.select("movie_id", explode(col("production_countries")).alias("production_countries"))
+
+df_movies_production_countries = df_movies_production_countries.select("movie_id", col("production_countries.*"))
+
+df_movies_production_countries = df_movies_production_countries.withColumn("iso_3166_1", when(col("iso_3166_1").isNull(), "XX")
+                                             .otherwise(col("iso_3166_1")))
+
+
+movies_spoken_languages_schema = ArrayType(StructType([
+        StructField("iso_639_1", StringType(), True),
+        StructField("name", StringType(), True)
+]))
+
+df_movie_spoken_languages = df_movies.withColumn("spoken_languages", from_json(col("spoken_languages"), movies_spoken_languages_schema))
+
+df_movie_spoken_languages = df_movie_spoken_languages.select("movie_id", explode(col("spoken_languages")).alias("spoken_languages"))
+
+df_movie_spoken_languages = df_movie_spoken_languages.select("movie_id", col("spoken_languages.*"))
+
+df_movie_spoken_languages = df_movie_spoken_languages.withColumn("iso_639_1", when(col("iso_639_1").isNull(), "XX")
+                                             .otherwise(col("iso_639_1")))
+
+
+## LOAD
+
+dataframes = [df_movies_movies, df_movies_genres, df_movies_keywords, 
+              df_movies_production_companies, df_movies_production_countries,
+              df_movie_spoken_languages]
+
+table_folders = ["MoviesMovies", "MoviesGenres", "MoviesKeywords", 
+                  "MoviesProductionCompanies", "MoviesProductionCountries",
+                  "MovieSpokenLanguages"]
+
+for i, dataframe in enumerate(dataframes):
+    time.sleep(5)
+    table_folder_name = table_folders[i]
+    dataframe.write.format("delta").mode("overwrite").save(f"s3a://tmdb-silver/{table_folder_name}")
+
+spark.stop()
